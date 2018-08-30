c0d882dded19b1188bd12b0e7243b02f6d720131
trns1997 <trns@ust.hk>
2018-08-30 12:41:17 +0800

    brushless + bicep update

--- emg_rms/emg_rms.ino
+++ emg_rms/emg_rms.ino
@@ -7,39 +7,37 @@ FilterOnePole lowpassFilter( LOWPASS, filterFrequency );
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  pinMode(11,OUTPUT);
+  pinMode(11, OUTPUT);
 }
 
 int i = 0;
 const int Isize = 10;
 int inputs[Isize] = {};
-int outRMS = 0;
+long outRMS = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
   adc = analogRead(A1) - 305;
   out = (EMA_b * lowpassFilter.input(abs(adc))) + ((1 - EMA_b) * out);
-  //Serial.print(out);
-//Serial.print(",");
+//  Serial.print(out);
+//  Serial.print(",");
 
   inputs[i] = sq(lowpassFilter.input(adc));
   outRMS = 0;
-  for(int j=0;j<Isize;j++){
+  for (int j = 0; j < Isize; j++) {
     outRMS += inputs[j];
   }
-  outRMS = sqrt(outRMS/Isize);
+  outRMS = sqrt(outRMS / Isize);
   i++;
-  if (i>Isize){
-    i=0;
+  if (i > Isize) {
+    i = 0;
   }
-  //Serial.println(outRMS);
+
+  outRMS = ((outRMS * 3) > 50) ? 49 : outRMS * 3;
+  analogWrite(11, map(outRMS, 0, 50, 127, 255));
 
 
-  outRMS = ((outRMS*3)>50)?49:outRMS*3;
-  analogWrite(11,map(outRMS,0,50,127,255));
 
-
-  
-  Serial.println(map(outRMS*3,0,50*2,127,255));
+  Serial.println(map(outRMS, 0, 50, 127, 255));
   delay(10);
 }
