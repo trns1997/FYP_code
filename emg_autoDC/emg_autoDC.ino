ba7a25d507acb97e50c1e261442e08e1908a2625
aphchan <andrechanph@gmail.com>
2018-09-20 23:31:55 +0800

    autoDC

--- emg_autoDC/emg_autoDC.ino
+++ emg_autoDC/emg_autoDC.ino
@@ -4,7 +4,7 @@ const int bufSize = 400;
 int inputs[bufSize] = {0};
 int pointer, i = 0;
 unsigned long avg, sum = 0;
-double ans, ans2, out = 0;
+double ans = 0;
 const double EMA_b = 0.4;
 void setup() {
   // put your setup code here, to run once:
@@ -15,36 +15,25 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if ( (micros() - timer) > 10) {
+  if ( (micros() - timer) > 30) {
     //Serial.println(micros()-timer);
     inputs[pointer] = analogRead(pin[0]);
-    //Serial.println(inputs[pointer]);
+    Serial.println(inputs[pointer]);
     if (pointer == bufSize - 1) {
       sum = 0;
       for (i = 0; i < bufSize; i++) {
         avg += inputs[i];
-        //Serial.print(inputs[i]);
-        //Serial.print(" ");
       }
-      //Serial.println();
       avg /= bufSize;
       //Serial.println(avg);
       for (i = 0; i < bufSize; i++) {
         inputs[i] = inputs[i] - avg;
         sum += pow(inputs[i], 2);
-        //sum2 = pow(inputs[i]+avg-304,2);
       }
       ans = sqrt(sum / bufSize);
-      out = (EMA_b * ans + ((1 - EMA_b) * out));
-      //ans2 = sqrt(sum2/bufSize);
-      Serial.println(out);
-      //Serial.print(",");
-      //Serial.println(ans2);
-      //Serial.print(",");
-      //Serial.println(ans*10);
+      Serial.println(ans);
+      //out = (EMA_b * ans + ((1 - EMA_b) * out));
       pointer = -1;
-      //Serial.print("b:");
-      //Serial.println(ans);
     }
 
     pointer++;
