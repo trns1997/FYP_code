9fad19178113eba5ce14844a6e5f46e430252702
aphchan <andrechanph@gmail.com>
2019-02-11 16:23:53 +0800

    Update data rate

--- arduino/2ndVer/2ndVer.ino
+++ arduino/2ndVer/2ndVer.ino
@@ -11,51 +11,53 @@
 * PA3 - A3 - Posterior
 * Output:
 * SDA, SCL - I2C
+* 
+* TODO:
+* DMA - DualMode - Continuous Conversion
 */
 
-unsigned long timer = 0;
-const int pinNum = 4;
-const int Rate = 1000;
-int pin[pinNum] = {PA0, PA1, PA2, PA3};
-const int bufSize = 20;
-unsigned int pointer, i = 0;
 
+#include <Wire_slave.h>
+#define SLAVE_ADDRESS 0x0A
+
+#define DEBUG 0
+
+unsigned long timer = 0;
+const int Rate = 500;
+const int pin[] = {PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7};
+const int pinNum = sizeof(pin)/sizeof(int);
+const int bufSize = 20;
+
+unsigned int pointer, i = 0;
 int datum = 0;
 float oldM[pinNum] = {0};
 float M[pinNum] = {0};
 float S[pinNum] = {0};
 
-//unsigned long temp_timer = 0;
-
-#include <Wire_slave.h>
-#define SLAVE_ADDRESS 0x0A
+#if DEBUG
+unsigned long temp_timer = 0;
+#endif
 
 union cvd{
   float val;
   byte b[4];
 };
-union cvd out[4] = {0};
+union cvd out[pinNum] = {0};
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  //pinMode(11, OUTPUT);
+
+  #if DEBUG
+    Serial.begin(115200);
+  #endif
+
   timer = micros();
   for (i=0;i<pinNum;i++){
     pinMode(pin[i],INPUT_ANALOG);
   }
   Wire.begin(SLAVE_ADDRESS);
-  //Wire.onReceive(receiveEvent);
   Wire.onRequest(sendData);
 }
-
-//void receiveEvent(int bytes){
-//  while (Wire.available()){
-//    int data = Wire.read();
-//    //Serial.print("data received: ");
-//    //Serial.println(data);
-//  }
-//}
 
 void sendData(){
   for (i=0;i<pinNum;i++){
@@ -66,23 +68,35 @@ void sendData(){
 void loop() {
   // put your main code here, to run repeatedly:
   if ( (micros() - timer) > Rate) {
-    //temp_timer = micros();
+    #if DEBUG
+      temp_timer = micros();
+    #endif
     timer = micros();
-    for (i=0;i<pinNum;i++){
+    for (i=0;i<pinNum/2;i++){
       datum = analogRead(pin[i]);
       oldM[i] = M[i];
       M[i] = M[i]+(datum-M[i])/(pointer+1);
       S[i] = S[i]+(datum-M[i])*(datum-oldM[i]);
     }
+    for (i=pinNum/2;i<pinNum;i++){
+      datum = analogRead(pin[i]);
+      S[i] = S[i]+datum*datum;
+    }
+    
     if (pointer >= bufSize-1){
       for (i=0;i<pinNum;i++){
         out[i].val = sqrt(S[i]/bufSize);
         M[i] = 0;
         S[i] = 0;
       }
+      #if DEBUG
+      //Serial.println(String(out[4].val)+","+String(out[5].val));//+","+String(out[4].val)+","+String(out[5].val));
+      #endif
       pointer = -1;
     }
     pointer++;
-    //Serial.println(micros()-temp_timer);
+    #if DEBUG
+      Serial.println(micros()-temp_timer);
+    #endif
   }
 }
